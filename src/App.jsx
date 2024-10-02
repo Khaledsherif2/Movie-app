ad57502c8535be7014507f9c82a12ad58588cbb9
Khaledsherif2 <khaled.dherif220@gmail.com>
2024-10-02 16:13:22 +0300

    updates and add contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About/About";
 import Footer from "./components/Footer/Footer";
 import Login from "./pages/Register-Login/Login";
 import Profile from "./pages/Profile/Profile";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Navbar />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
